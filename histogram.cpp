f50a91f42a707020b65ab1ccfc48b5554a6516f9
zsh <kevin_201353@126.com>
2025-08-22 18:01:51 +0800

    update

--- histogram.cpp
+++ histogram.cpp
@@ -3,11 +3,13 @@
 #include <QPushButton>
 #include <QVBoxLayout>
 #include <QMessageBox>
+#include <QPolygonF>
 #include "mainwindow.h"
 
 HistoGram::HistoGram(QWidget *parent) : QWidget(parent)
 {
     //this->setWindowFlags(Qt::CustomizeWindowHint | Qt::FramelessWindowHint);
+    /*
     this->setStyleSheet("background-color:rgb(255,150,250)");
     //QLabel *label = new QLabel("This is my custom widget!", this);
     QVBoxLayout *layout = new QVBoxLayout(this);
@@ -25,6 +27,7 @@ HistoGram::HistoGram(QWidget *parent) : QWidget(parent)
     layout->addWidget(button2);
     setLayout(layout);
     connect(button1, &QPushButton::clicked, this, &OnCheckWindow);
+    */
 }
 
 HistoGram::~HistoGram()
@@ -34,4 +37,126 @@ HistoGram::~HistoGram()
 void HistoGram::OnCheckWindow()
 {
     emit hotKeyPressed();
+}
+
+void HistoGram::paintEvent(QPaintEvent *event)
+{
+    DrawX1();
+}
+
+void HistoGram::mouseMoveEvent(QMouseEvent *event)
+{
+    int entered = -1;
+    int count = (int)bins.size();
+    for (int i = 0; i < count; i++)
+    {
+        if (rects[i].contains(event->pos()))
+        {
+            entered = i;
+            break;
+        }
+    }
+    if (entered != hitBin)
+    {
+        setToolTip((entered >= 0) ? QString(u8"值：%1").arg(bins[entered]) : QString());
+        hitBin = entered;
+        update();
+    }
+}
+
+
+void HistoGram::enterEvent(QEvent *event)
+{
+
+}
+
+void HistoGram::leaveEvent(QEvent *event)
+{
+
+}
+
+int HistoGram::calcNearInteger(int value, bool upperOrLower)
+{
+    int i = 10;
+    while (value / i != 0)
+    {
+        i *= 10;
+    }
+    i = qMax(i / 10, 10);
+    int x = value / i;
+    if (upperOrLower)
+    {
+        return i * (x + 1);
+    }
+    return i * x;
+}
+
+void HistoGram::DrawX1()
+{
+    QPainter painter(this);
+
+    painter.setPen(Qt::NoPen);
+    painter.setBrush(Qt::black);
+    painter.drawRect(rect());
+
+    painter.setRenderHint(QPainter::Antialiasing);
+    painter.setBrush(Qt::NoBrush);
+    painter.setPen(Qt::black);
+    const QMargins margin(10, 20, 20, 20);
+
+    QPen pen;
+    pen.setWidth(3);
+    pen.setColor(QColor(255,0,0));
+    pen.setStyle(Qt::SolidLine);
+    painter.setPen(pen);
+
+
+    QBrush brush;
+    brush.setColor(Qt::black);
+    brush.setStyle(Qt::Dense6Pattern);
+    painter.setBrush(brush);
+    painter.drawRect(margin.left(),margin.top(),100,50);
+    /*
+    painter.setPen(pen);
+    painter.drawLine(50,50,150,50);
+    painter.drawLine(50,50,50,150);
+
+    QBrush brush;
+    brush.setColor(Qt::black);
+    brush.setStyle(Qt::Dense6Pattern);
+    painter.setBrush(brush);
+    painter.drawRect(150,150,100,50);
+
+    painter.drawEllipse(250,250,40,40);
+    */
+
+    painter.setPen(pen);
+    QPointF startPoint(200,200);
+    QPointF endPoint(300,200);
+    //绘制直线
+    painter.drawLine(startPoint,endPoint);
+    drawArrow(painter,startPoint,endPoint);
+    painter.end();
+}
+
+void HistoGram::drawArrow(QPainter &painter, const QPointF &start, const QPointF &end)
+{
+    //箭头大小
+    double arrowSize = 15;
+    //计算直线的角度
+    double angle = std::atan2(end.y()-start.y(), end.x()-start.x());
+    //计算箭头两个点的位置
+    QPointF  arrowP1 = end - QPointF(std::cos(angle-M_PI/6)*arrowSize,std::sin(angle - M_PI / 6) * arrowSize);
+    QPointF  arrowP2 = end - QPointF(std::cos(angle+M_PI/6)*arrowSize,std::sin(angle + M_PI / 6) * arrowSize);
+    //保存当前画笔
+  //  QPen savePen = painter.pen();
+    //设置箭头填充颜色
+  //  QBrush brush(savePen.color(), Qt::SolidPattern);
+  //  painter.setBrush(brush);
+    //绘制箭头三角形
+    QPolygonF  arrowHead;
+    arrowHead<<end<<arrowP1<<arrowP2;
+    painter.drawPolygon(arrowHead);
+    //恢复画笔
+    painter.setBrush(Qt::NoBrush);
 }
--- histogram.h
+++ histogram.h
@@ -17,8 +17,19 @@ signals:
 private slots:
     void OnCheckWindow();
 private:
+    void paintEvent(QPaintEvent *event) override;
+    void mouseMoveEvent(QMouseEvent *event) override;
+    void enterEvent(QEvent *event) override;
+    void leaveEvent(QEvent *event) override;
+    int  calcNearInteger(int value, bool upperOrLower);
+    void DrawX1();
+    void drawArrow(QPainter &painter, const QPointF &start, const QPointF &end);
+private:
     QPushButton *button1;
     QPushButton *button2;
+    QVector<int> bins;
+    QVector<QRectF> rects;
+    int hitBin;
 };
 
 #endif // HISTOGRAM_H
